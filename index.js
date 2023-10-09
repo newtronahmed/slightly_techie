fdb61b90576ea48e2ada34bca67efb6836cceb43
neutronahmed <bazubairu@st.ug.edu.gh>
2023-10-09 14:13:34 +0000

    conditionally install dotenve

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const blogRouter = require('./routes/blog')
 const app = express();
-const mongoose = require('mongoose');
-require("dotenv").config()
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV != "production")
+{
+    require("dotenv").config()
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/blog', blogRouter)
